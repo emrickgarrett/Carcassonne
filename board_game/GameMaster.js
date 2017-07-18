890eba0db5fcc827ef8846e3c53108e279dcfc29
emrickgarrett <emrickgj@miamioh.edu>
2017-07-17 23:17:50 -0600

    Added code that will check the gameboard for when pieces are being hovered, and return that piece if it is clicked. Works great for now, may need to update it later if I find that it causes lag (seems absolutely fine atm)

--- board_game/GameMaster.js
+++ board_game/GameMaster.js
@@ -15,15 +15,20 @@ class GameMaster {
 		this.players = [];
 		this.curTurn = 0;
 		this.arePlaying = true;
+		this.gamePlaying = false;
 
 
 		this.gameBoard = new GameBoard(null, 1, this.canvas, this.context);
-		this.playerInputController = new PlayerInputController(this.canvas, this.context, this.dom);
+		this.playerInputController = new PlayerInputController(this.gameBoard, this.canvas, this.context, this.dom);
 	}
 
 	startGame(){
 		this.gameBoard.placeAllPieces();
 		this.gameBoard.render();
+		
+
+		//Start game, game fps 60 by default
+		this.loopId = setInterval(this.gameLoop.bind(this), 1000 / this.fps);
 	}
 
 
@@ -31,8 +36,10 @@ class GameMaster {
 		//do stuff with the dom
 	}
 
-	gameLoop(){
+	gameLoop(GameMaster){
 		//No turn logic/placing logic for EVERYONE, just the local player. Game loop will have to be server side
+		//console.log(this);
+		this.playerInputController.updateMouse();
 	}
 
 }
--- board_game/PlayerInputController.js
+++ board_game/PlayerInputController.js
@@ -1,11 +1,15 @@
 
 class PlayerInputController{
 	
-	constructor(canvas, context, dom){
+	constructor(gameboard, canvas, context, dom){
+		this.gameboard = gameboard;
 		this.canvas = canvas;
 		this.context = context;
 		this.dom = dom;
-
+		this.hovering = false;
+		this.curObjectHover = null;
+		this.dateObject = new Date();
+		this.lastCheck = this.dateObject.getMilliseconds();
 		this.registerController();
 
 	}
@@ -14,6 +18,9 @@ class PlayerInputController{
 		this.canvas.addEventListener('mouseup', this.mouseClicked, false);
 		this.dom.addEventListener('keyup', this.keyPressed);
 		this.canvas.addEventListener('mousemove', this.mouseMoved);
+
+		this.canvas.inputController = this;
+		this.dom.inputController = this;
 	}
 
 	mouseClicked(event){
@@ -26,11 +33,22 @@ class PlayerInputController{
 		var canvasY = event.layerY;
 
 		console.log("mouseXY: " + mouseX + " " + mouseY + " canvasXY: " + canvasX + " " + canvasY);
+
+		if(this.inputController.curObjectHover !== null){
+			console.log(this.inputController.curObjectHover);
+		}
 	}
 
 	mouseMoved(event){
 		//Need a way to determine if they are hovering a piece or not... maybe an array of
 		//locations to keep track off?
+
+		//Idk if necessary... just to stop the object from constantly blasting/causing lag
+		if(this.inputController.dateObject.getMilliseconds() - this.inputController.lastCheck >= 300){
+			return;
+		}
+		this.inputController.lastCheck = this.inputController.dateObject.getMilliseconds();
+
 
 		//I believe this will be top left of dom, will have to confirm
 		var mouseX = event.clientX;
@@ -42,11 +60,40 @@ class PlayerInputController{
 
 		//Too spammy... may want this on at some point tho
 		//console.log(event);
+
+		//Simply loop through every game object based on mouse x/y and see if
+		//it's hovering over an object. Shouldn't be too bad with ~50-100 objects.
+
+		//pieces first
+		var pieces = this.inputController.gameboard.pieces;
+
+		for(var i = 0; i < pieces.length; i++){
+			if(canvasX >= pieces[i].x*70 && canvasX <= pieces[i].x*70+70
+				&& canvasY >= pieces[i].y*70 && canvasY <= pieces[i].y*70+70){
+					this.inputController.hovering = true;
+					this.inputController.curObjectHover = pieces[i];
+					return;
+			}
+		}
+
+		this.inputController.curObjectHover = null;
+		this.inputController.hovering = false;
+
 	}
 
 	keyPressed(event){
 		var key = event.key;
 		console.log(key);
+	}
+
+	updateMouse(){
+		if(this.hovering){
+			//pointer
+			this.canvas.style.cursor="pointer";
+		}else{
+			//default
+			this.canvas.style.cursor="default";
+		}
 	}
 }
 
